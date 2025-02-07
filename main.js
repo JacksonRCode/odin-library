bff0d54f09744be394c90151bcf8bd60702be542
JacksonRCode <jackdareid@gmail.com>
2025-02-07 15:25:14 -0500

    Applied basic styling and implemented javascript functions granting the user control over their library.

---
+++ main.js
@@ -0,0 +1,102 @@
+
+// const bookList = [];
+// const bookList = [1,2,3,4,5,6];
+
+function Book(title, author, pageCount, releaseDate, dateAdded) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.releaseDate = releaseDate;
+  this.addDate = dateAdded;
+}
+
+let book1 = new Book('Yay', 'Yay', '22', 'Feb3rd')
+const bookList = [book1];
+
+
+function createBook() {
+  // Ask for details about the book
+
+  return 1;
+}
+
+function addToBookList(bookList) {
+  let book = createBook();
+
+
+  if (bookList.push(book)) {
+  alert("Successfully added book to book list");
+  } else {
+    alert("Failed to add book to library");
+  } 
+}
+
+function removeBook(book) {
+  offset = 0;
+  let l = bookList.length;
+
+  for (let i = 1; i < l; i++) {
+    if (bookList[i-1] === book) {
+      offset = 1;
+    }
+    bookList[i - 1] = bookList[i - 1 + offset];
+  }
+
+  if (offset || bookList[l-1] === book) {
+    bookList[l-1] = null;
+  }
+}
+
+function displayBooks(bookList) {
+  // Get library from html
+  const libraryDiv = document.querySelector('.library');
+
+  let l = bookList.length;
+  for (let i = 0; i < l; i++) {
+    /* Steps:
+      1. Create new div and give it a class of .bookCard
+      2. Create h2 element for the book title
+      3. Create p element for the rest of the book elements
+      4. Add each of these elements to the newDiv as children
+      5. Add newDiv to library as child
+    */
+    let book = bookList[i];
+
+    // Create div and give it class
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('bookCard');
+
+    // Title
+    const h2Title = document.createElement('h2');
+    h2Title.classList.add('cardTitle');
+    h2Title.textContent = book.title;
+
+    // Author
+    const pAuthor = document.createElement('p');
+    pAuthor.textContent = book.author;
+
+    // Pagecount
+    const pPageCount = document.createElement('p');
+    pPageCount.textContent = book.pageCount; 
+
+    // Release Date
+    const pReleaseDate = document.createElement('p');
+    pReleaseDate.textContent = book.releaseDate;
+
+    // Date Added
+    const pDateAdded = document.createElement('p');
+    pDateAdded.textContent = book.dateAdded;
+
+    // Add children to newDiv
+    newDiv.appendChild(h2Title);
+    newDiv.appendChild(pAuthor);
+    newDiv.appendChild(pPageCount);
+    newDiv.appendChild(pReleaseDate);
+    newDiv.appendChild(pDateAdded);
+    
+    // Add newDiv to library
+    libraryDiv.appendChild(newDiv);
+  }
+}
+
+displayBooks(bookList);
